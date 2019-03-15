7259209b6180921c36409aa5ee41f25290f4a068
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-03-15 16:12:31 -0500

    cleaning up code

--- src/app/components/PortfolioCard/Back.tsx
+++ src/app/components/PortfolioCard/Back.tsx
@@ -1,65 +1,60 @@
 import * as React from 'react';
 require('./index.scss');
 
-  const Back = ({about, code, site, flipCard, ref}) => {
+  const Back = ({about, code, site, flipCard, ref, pic, name}) => {
 
     const [cardStyle, setCardStyle] = React.useState({});
     const [aboutStyle, setAboutStyle] = React.useState({});
+    const [cardSide, setCardSide] = React.useState("cardFront");
     const aboutRef = React.useRef(null);
 
+
   React.useEffect(() => {
-      const {clientHeight, clientWidth, scrollHeight} = aboutRef.current
-
-    console.log(clientHeight, scrollHeight)
-
-    if(clientHeight === scrollHeight){
-        console.log('no need')
-    }
-     else{
-      console.log('let resize')
-      let area = scrollHeight * clientWidth;
-
-      let newUnit = Math.sqrt(area);
-
-      let nHt = (newUnit/ newUnit) * ((88/100) * newUnit);
-
-      console.log(newUnit, nHt)
-
-            console.log('nH is', nHt)
-
-
-
-      const resizeText = {
-        height: newUnit,
-        width: newUnit
-      }
-
-        let grr = newUnit + ((10/100) * newUnit)
-
-    const wS = {
-      height: grr,
-      width: grr
-
-    }
-      setAboutStyle(resizeText);
-      setCardStyle(wS)
-
-    }
-
-
     }, []
   );
 
 
+  React.useLayoutEffect(()=>{
+      const {clientHeight, clientWidth, scrollHeight} = aboutRef.current;
+
+      if(cardSide === "cardBack" && clientHeight != scrollHeight ){
+          setAboutStyle({
+            height: Math.sqrt(scrollHeight * clientWidth),
+            width: Math.sqrt(scrollHeight * clientWidth)
+          });
+
+          setCardStyle({
+            height: Math.sqrt(scrollHeight * clientWidth) + ((10/100) * Math.sqrt(scrollHeight * clientWidth)),
+            width: Math.sqrt(scrollHeight * clientWidth) + ((10/100) * Math.sqrt(scrollHeight * clientWidth))
+          })
+      }
+  })
+
+
+flipCard = () => {
+    if(cardSide === "cardFront"){
+      setCardSide("cardBack")
+    }
+    else if (cardSide === "cardBack"){
+      setCardSide("cardFront")
+      setCardStyle({})
+      setAboutStyle({})
+    }
+  }
+
+
     return(
-      <div style={cardStyle} className='cardBack' >
+      <div style={cardStyle} className={cardSide} >
+        <button onClick={flipCard} className="info-button drum">i</button>
+
+        <img className='portfolio-pic' src={pic} />
+        <h1 className='project-title'>{name}</h1>
         <h2 style={aboutStyle} className="project-description" ref={aboutRef}>{about}</h2>
         <div className="navwrapper">
           <button onClick={flipCard} className="info-button">x</button>
-
           <a className="project-site" href={site} target="_blank">Site</a>
           <a className="project-source" href={code} target="_blank">Code</a>
-          <h4>Card Title</h4>
+          {/* <h4>Card Title</h4> */}
         </div>
       </div>
     )
--- src/app/components/PortfolioCard/index.tsx
+++ src/app/components/PortfolioCard/index.tsx
@@ -1,43 +1,60 @@
 import * as React from 'react';
 require('./index.scss');
 
-import Back from './Back'
-
-const Front = ({pic, name, flipCard, site, code}) =>
-    <div className='cardFront'>
-      <img id='portfolio-pic' src={pic} />
-      <h1 id='project-title'>{name}</h1>
-      <button onClick={flipCard} className="info-button">i</button>
-    </div>
+  const PortfolioCard = ({about, code, site, flipCard, ref, pic, name}) => {
+    const [cardStyle, setCardStyle] = React.useState({});
+    const [aboutStyle, setAboutStyle] = React.useState({});
+    const [cardSide, setCardSide] = React.useState("cardFront");
+    const aboutRef = React.useRef(null);
 
 
-class PortfolioCard extends React.Component<any, any>{
-  constructor(props: any){
-    super(props);
-// this.stepInput = React.createRef();
-    // this.myRef = React.createRef();
-    this.state = {
-      flip: false
+    React.useLayoutEffect(()=>{
+      const {clientHeight, clientWidth, scrollHeight} = aboutRef.current;
+
+      console.log(clientHeight, scrollHeight)
+
+
+      if(cardSide === "cardBack" && clientHeight != scrollHeight ){
+              console.log('effectin inside')
+
+
+          setAboutStyle({
+            height: Math.sqrt(scrollHeight * clientWidth),
+            width: Math.sqrt(scrollHeight * clientWidth)
+          });
+
+          setCardStyle({
+            height: Math.sqrt(scrollHeight * clientWidth) + ((10/100) * Math.sqrt(scrollHeight * clientWidth)),
+            width: Math.sqrt(scrollHeight * clientWidth) + ((10/100) * Math.sqrt(scrollHeight * clientWidth))
+          })
+      }
+    })
+
+
+    flipCard = () => {
+        if(cardSide === "cardFront"){
+          setCardSide("cardBack")
+        }
+        else if (cardSide === "cardBack"){
+          setCardSide("cardFront")
+          setCardStyle({})
+          setAboutStyle({})
+        }
     }
-  }
 
-  flipCard = () =>{
-    // console.log('ref is ', this.pDescribe);
-    this.setState({flip: !this.state.flip});
-
-}
-
-
-
-  render(){
-    const {about, code, site, pic, name} = this.props;
 
     return(
-    <React.Fragment>
-    {this.state.flip ? <Back  about={about} code={code} site={site} flipCard={this.flipCard} /> : <Front pic={pic} name={name} flipCard={this.flipCard}/>}
-    </React.Fragment>
+      <div style={cardStyle} className={cardSide} >
+        <div className="navwrapper">
+          <div onClick={flipCard} className="info-button">info</div>
+          <a className="project-site" href={site} target="_blank">Site</a>
+          <a className="project-source" href={code} target="_blank">Code</a>
+          <h1 className='project-title'>{name}</h1>
+        </div>
+        <img className='portfolio-pic' src={pic} />
+        <h2 style={aboutStyle} className="project-description" ref={aboutRef}>{about}</h2>
+      </div>
     )
-  }
 
 }
 
