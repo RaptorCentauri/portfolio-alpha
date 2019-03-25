693a03b5777e614cb41bfede79e7c4b1aa19cfcc
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-03-25 15:48:35 -0500

    general backups, using svgs

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,12 +1,10 @@
 import AboutMe from './AboutMe/aboutme';
 import Navbar from './Navbar/navbar'
 import Portfolio from './Portfolio/portfolio'
-import StayTuned from './StayTuned';
 
 
 export{
   Navbar,
   Portfolio,
-  StayTuned,
   AboutMe
 }
