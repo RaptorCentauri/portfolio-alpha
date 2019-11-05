fe9b1a11064a8bb5f27fbdec35d3af76f53b9960
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-11-05 10:11:31 -0600

    Changed About Me Pic and text

--- src/app/components/AboutMe/aboutme.tsx
+++ src/app/components/AboutMe/aboutme.tsx
@@ -8,9 +8,8 @@ import profilePic from './assets/images/profile_img.jpg'
 const AboutMe = (props) => {
 
   const aboutMe =
-  `Dave is a full-stack web developer currently living in Chicago, IL.
-  He possesses strong problem solving skills, an eye for design, and an excellent work ethic.
-  He believes that well maintained code and well designed interfaces are equally important, and strives to demonstrate that in his work.`
+  `Driven by curiosity, I am avid learner and always seeking unconventional solutions to existing problems.  This has led me to explore my fascination of the interactions between people and technology. With two years of JavaScript development experience, I combine analytical and logical approaches to deliver solutions that are attractive end-users and developers alike. Additionally, I leverage my excellent communication skills to bridge the gap of understanding between those with non-technical backgrounds and developers. I consider the strongest interfaces to be the ones that come naturally to the user, as if they have always been a part of his or her life.  When not coding you can find me hiking in a nearby forest, playing video games, or just spending time with friends. Additionally, as a classically trained singer, I have sung in languages such as French, Spanish, and Italian.
+`
 
 
   return (
